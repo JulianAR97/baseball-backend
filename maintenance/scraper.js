749e8e309ae632d6f83cf9b3ec2b208e3feaacfe
Julian <julianrosenthal@yahoo.com>
2021-07-19 11:49:44 -0700

    copy/pasted espn scraper code from my other repo

---
+++ maintenance/scraper.js
@@ -0,0 +1,136 @@
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+
+const getData = async (url) => {
+  console.log('Pulling data....')
+  
+  const website = await fetch(url)
+    .then(res => res.text())
+    .then(body => body)
+
+  const $ = cheerio.load(website);
+  const scripts = await $("script").toArray()
+
+  const scoreboard = scripts.find(
+    s => 
+      s.children[0] &&
+      s.children[0].data.includes("window.espn.scoreboardData")
+    ).children[0].data
+
+  let data = scoreboard
+    .replace("window.espn.scoreboardData", "")
+    .replace("=", '')
+    .replace(/;/g, '')
+    .split("window.espn.scoreboardSettings")[0]
+    .trim();
+
+  data = JSON.parse(data)
+  const { events } = data
+  return events;
+}
+
+// Parsing Functions
+
+const getScores = async (url) => {
+  const data = await getData(url)
+  console.log('Parsing scores...')
+  let parsed = [];
+  
+  for (let game of data) {
+    const name = game.shortName
+    const competitors = game.competitions[0].competitors
+    const homeRuns = competitors.find(c => c.homeAway === 'home').score
+    const awayRuns = competitors.find(c => c.homeAway === 'away').score
+    const homeName = name.split('@')[1].trim()
+    const awayName = name.split('@')[0].trim()
+    const detail = game.status.type.detail
+    const dateTime = dateToEST(game.competitions[0].date)
+    const {date, time} = dateTime
+    const score = { [awayName]: awayRuns, [homeName]: homeRuns }
+    const status = 
+      game.status.type.description === 'Final' ? 
+      game.status.type.detail :
+      game.status.type.description
+    
+    const scoreInfo = {name, score, date, time, status}
+    parsed.push(scoreInfo)
+  
+  }
+
+  console.log('Done')
+  return parsed;
+}
+
+const getOdds = async (url) => {
+  const data = await getData(url)
+  console.log('Parsing odds...')
+  let parsed = []
+  
+  for (let game of data) {
+    const name = game.shortName
+    const odds = game.competitions[0].odds
+    const moneyLine = odds[0].details || 'n/a'
+    const overUnder = odds[0].overUnder || 'n/a'
+    const oddsInfo = {name, moneyLine, overUnder}
+    parsed.push(oddsInfo)
+  }
+
+  console.log('Done')
+  return parsed;
+}
+
+// Helpers
+const dateToEST = (date) => {
+  date = new Date(
+    Date.parse(date)
+  )
+
+  let dateObj = {}
+  
+  const options = { 
+    time: {
+      timeZone: 'America/New_York',
+      timeZoneName: 'short',
+      hour: '2-digit',
+      minute: '2-digit'
+    },
+    date: {
+      timeZone: 'America/New_York', 
+      year: 'numeric', 
+      month: '2-digit', 
+      day: '2-digit'
+    }
+  }
+
+  dateObj.date = date.toLocaleDateString('en-US', options.date)
+  dateObj.time = date.toLocaleTimeString('en-US', options.time)
+  
+  return dateObj
+}
+
+
+
+// Run
+
+const scrapeScoreboard = async (params) => {
+  const baseURL = "https://www.espn.com/mlb/scoreboard"
+  let data;
+  switch (params.type) {
+    case 'ODDS':
+      data = await getOdds(baseURL)
+      break;
+    
+    case 'SCOREBOARD':
+      let url =
+        params.date ?
+        baseURL + `/_/date/${date}` :
+        baseURL
+
+      data = await getScores(url)
+
+    default:
+      break;
+  }
+
+  return data;
+}
