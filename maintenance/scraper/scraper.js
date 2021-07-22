93b9596953af37bfaccb5ed542069fe654d34550
Julian <julianrosenthal@yahoo.com>
2021-07-22 16:47:11 -0700

    imported bbref scraper

--- maintenance/scraper/scraper.js
+++ maintenance/scraper/scraper.js
@@ -1,5 +1,6 @@
 import { scrapeBoxscore as boxscore } from "./boxscore.js";
 import { getScores as scores, getOdds as odds } from "./scoreboard.js";
+import { scrapeStats as stats } from './bbRef.js'
 
 
-export default {boxscore, scores, odds}
+export default {boxscore, scores, odds, stats}
